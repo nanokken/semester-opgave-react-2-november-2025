30385d8e8e5787421cd4d079ac99bd520b04699b
nanokken <asge2055@mcdmstudent.dk>
2025-11-03 10:56:54 +0100

    start setup
    continue work at home

--- glamping-main/sites/www/src/App.jsx
+++ glamping-main/sites/www/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import styles from './App.module.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className={styles.App} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className={styles.App} alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/ophold" />
+          <Route path='/ophold/:id' />
+          <Route path="/contact" />
+          <Route path="/aktiviteter" />
+        </Routes>
+        </Router>
     </>
   )
 }
